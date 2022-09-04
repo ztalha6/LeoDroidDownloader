26be5b8a5188ff576900af7e17a177891f6c21a1
Muhammad Talha Siddique <ztalha6@gmail.com>
2022-09-04 22:47:07 +0500

    testing settings

--- lib/ad_unit_ids/ad_unit_id.dart
+++ lib/ad_unit_ids/ad_unit_id.dart
@@ -1,9 +1,9 @@
 import 'dart:io';
 
 class AdUnitId {
-  static String banner =
-      Platform.isAndroid ? 'ca-app-pub-6054901892311065/2427354537' : '';
+  static String banner = Platform.isAndroid ? 'ca-app-pub-6054901892311065/8754349885' : '';
   static String adManagerBanner = Platform.isAndroid ? '' : '';
   static String interstitial = Platform.isAndroid ? '' : '';
   static String rewarded = Platform.isAndroid ? '' : '';
 }
+
